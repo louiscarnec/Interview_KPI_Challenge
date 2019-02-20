86c4723de0f08147c6581b43cfb2bd8f5b0cdf2f
Louis Carnec <louis@codacy.com>
2019-02-20 16:13:27 +0000

    fixed issues

--- tttt.py
+++ tttt.py
@@ -1,4 +1,4 @@
-print('first issue; too many characters!!!')
+print('f')
 
 #adding extra keys
 print("%(PARG_1)d %(PARG_2)d" % {'PARG_1': 1, 'PARG_2':2, 'PARG_3':3})
@@ -11,22 +11,22 @@ for index in range(len(obj)):
 
 #conside enumeration
 obj = [1,2,3]
-for index in range(len(obj)):
-    value = obj[index]
-    print(value)    
+for index, value in enumerate(obj):
+    print(index, value) 
 
 # exception
 try:
     1 / 0
 except Exception:
-    print("exception occurred")
+    print("ex")
 except ZeroDivisionError:
     print("don't divide numbers by 0!")
 
 # exception
 try:
     1 / 0
+except ZeroDivisionError:
+    print("don't divide numbers by 0!")  
 except Exception:
     print("exception occurred")
-except ZeroDivisionError:
-    print("don't divide numbers by 0!")    
+  
