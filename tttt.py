db1a8bea9f10df5b6d99e86060c58b790eb2e720
Louis Carnec <louis@codacy.com>
2019-02-21 17:22:14 +0000

    fix duplication

--- tttt.py
+++ tttt.py
@@ -21,44 +21,12 @@ def something():
     #adding extra keys
     print("%(PARG_1)d %(PARG_2)d" % {'PARG_1': 1, 'PARG_2':2, 'PARG_3':3})
 
-def something1():
-    #adding extra keys
-    print("%(PARG_1)d %(PARG_2)d" % {'PARG_1': 1, 'PARG_2':2, 'PARG_3':3})
-    #adding extra keys
-    print("%(PARG_1)d %(PARG_2)d" % {'PARG_1': 1, 'PARG_2':2, 'PARG_3':3})
-    #adding extra keys
-    print("%(PARG_1)d %(PARG_2)d" % {'PARG_1': 1, 'PARG_2':2, 'PARG_3':3})
-    #adding extra keys
-    print("%(PARG_1)d %(PARG_2)d" % {'PARG_1': 1, 'PARG_2':2, 'PARG_3':3})
-    #adding extra keys
-    print("%(PARG_1)d %(PARG_2)d" % {'PARG_1': 1, 'PARG_2':2, 'PARG_3':3})
-    #adding extra keys
-    print("%(PARG_1)d %(PARG_2)d" % {'PARG_1': 1, 'PARG_2':2, 'PARG_3':3})
-    #adding extra keys
-    print("%(PARG_1)d %(PARG_2)d" % {'PARG_1': 1, 'PARG_2':2, 'PARG_3':3})
 
 #conside enumeration
 obj = [1,2,3]
 for index, value in enumerate(obj):
     print(index, value) 
 
-#conside enumeration
-obj = [1,2,3]
-for index in range(len(obj)):
-    value = obj[index]
-    print(value)
-
-#conside enumeration
-obj = [1,2,3]
-for index in range(len(obj)):
-    value = obj[index]
-    print(value)
-
-#conside enumeration
-obj = [1,2,3]
-for index in range(len(obj)):
-    value = obj[index]
-    print(value)
 
 #conside enumeration
 obj = [1,2,3]
