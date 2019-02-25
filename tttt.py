c75d8a8e5dbbe14f462f7896ca0722ba1bcf178d
Louis Carnec <louis@codacy.com>
2019-02-25 11:43:30 +0000

    fix issues

--- tttt.py
+++ tttt.py
@@ -20,146 +20,34 @@ def something():
     print("%(PARG_1)d %(PARG_2)d" % {'PARG_1': 1, 'PARG_2':2, 'PARG_3':3})
     #adding extra keys
     print("%(PARG_1)d %(PARG_2)d" % {'PARG_1': 1, 'PARG_2':2, 'PARG_3':3})
-
-
-#adding extra keys
-print("%(PARG_1)d %(PARG_2)d" % {'PARG_1': 1, 'PARG_2':2, 'PARG_3':3})
-def something():
-    #adding extra keys
-    print("%(PARG_1)d %(PARG_2)d" % {'PARG_1': 1, 'PARG_2':2, 'PARG_3':3})
-    #adding extra keys
-    print("%(PARG_1)d %(PARG_2)d" % {'PARG_1': 1, 'PARG_2':2, 'PARG_3':3})
-    #adding extra keys
-    print("%(PARG_1)d %(PARG_2)d" % {'PARG_1': 1, 'PARG_2':2, 'PARG_3':3})
-    #adding extra keys
-    print("%(PARG_1)d %(PARG_2)d" % {'PARG_1': 1, 'PARG_2':2, 'PARG_3':3})
-    #adding extra keys
-    print("%(PARG_1)d %(PARG_2)d" % {'PARG_1': 1, 'PARG_2':2, 'PARG_3':3})
-    #adding extra keys
-    print("%(PARG_1)d %(PARG_2)d" % {'PARG_1': 1, 'PARG_2':2, 'PARG_3':3})
-    #adding extra keys
-    print("%(PARG_1)d %(PARG_2)d" % {'PARG_1': 1, 'PARG_2':2, 'PARG_3':3})
-
-
-#adding extra keys
-print("%(PARG_1)d %(PARG_2)d" % {'PARG_1': 1, 'PARG_2':2, 'PARG_3':3})
-def something():
-    #adding extra keys
-    print("%(PARG_1)d %(PARG_2)d" % {'PARG_1': 1, 'PARG_2':2, 'PARG_3':3})
-    #adding extra keys
-    print("%(PARG_1)d %(PARG_2)d" % {'PARG_1': 1, 'PARG_2':2, 'PARG_3':3})
-    #adding extra keys
-    print("%(PARG_1)d %(PARG_2)d" % {'PARG_1': 1, 'PARG_2':2, 'PARG_3':3})
-    #adding extra keys
-    print("%(PARG_1)d %(PARG_2)d" % {'PARG_1': 1, 'PARG_2':2, 'PARG_3':3})
-    #adding extra keys
-    print("%(PARG_1)d %(PARG_2)d" % {'PARG_1': 1, 'PARG_2':2, 'PARG_3':3})
-    #adding extra keys
-    print("%(PARG_1)d %(PARG_2)d" % {'PARG_1': 1, 'PARG_2':2, 'PARG_3':3})
-    #adding extra keys
-    print("%(PARG_1)d %(PARG_2)d" % {'PARG_1': 1, 'PARG_2':2, 'PARG_3':3})
-
-
-#adding extra keys
-print("%(PARG_1)d %(PARG_2)d" % {'PARG_1': 1, 'PARG_2':2, 'PARG_3':3})
-def something():
-    #adding extra keys
-    print("%(PARG_1)d %(PARG_2)d" % {'PARG_1': 1, 'PARG_2':2, 'PARG_3':3})
-    #adding extra keys
-    print("%(PARG_1)d %(PARG_2)d" % {'PARG_1': 1, 'PARG_2':2, 'PARG_3':3})
-    #adding extra keys
-    print("%(PARG_1)d %(PARG_2)d" % {'PARG_1': 1, 'PARG_2':2, 'PARG_3':3})
-    #adding extra keys
-    print("%(PARG_1)d %(PARG_2)d" % {'PARG_1': 1, 'PARG_2':2, 'PARG_3':3})
-    #adding extra keys
-    print("%(PARG_1)d %(PARG_2)d" % {'PARG_1': 1, 'PARG_2':2, 'PARG_3':3})
-    #adding extra keys
-    print("%(PARG_1)d %(PARG_2)d" % {'PARG_1': 1, 'PARG_2':2, 'PARG_3':3})
-    #adding extra keys
-    print("%(PARG_1)d %(PARG_2)d" % {'PARG_1': 1, 'PARG_2':2, 'PARG_3':3})
-
-
-#conside enumeration
-obj = [1,2,3]
-for index, value in enumerate(obj):
-    print(index, value) 
-
-
-#conside enumeration
-obj = [1,2,3]
-for index in range(len(obj)):
-    value = obj[index]
-    print(value)
-
-#conside enumeration
-obj = [1,2,3]
-for index in range(len(obj)):
-    value = obj[index]
-    print(value)
-
-#conside enumeration
-obj = [1,2,3]
-for index, value in enumerate(obj):
-    print(index, value) 
-
-# exception
-try:
-    1 / 0
-except ZeroDivisionError:
-    print("don't divide numbers by 0!")    
-except Exception:
-    print("ex")
-
-try:
-    1 / 0
-except ZeroDivisionError:
-    print("don't divide numbers by 0!")    
-except Exception:
-    print("ex")
-
-try:
-    1 / 0
-except ZeroDivisionError:
-    print("don't divide numbers by 0!")    
-except Exception:
-    print("ex")        
-
-# exception
-try:
-    1 / 0
-except Exception:
-    print("exception occurred")    
-except ZeroDivisionError:
-    print("don't divide numbers by 0!")  
-
-  
-# exception
-try:
-    1 / 0
-except ZeroDivisionError:
-    print("don't divide numbers by 0!")  
-except Exception:
-    print("exception occurred")
     
+#conside enumeration
+obj = [1,2,3]
+for index, value in enumerate(obj):
+    print(index, value) 
+
+
+#conside enumeration
+obj = [1,2,3]
+for index in range(len(obj)):
+    value = obj[index]
+    print(value)
+
+#conside enumeration
+obj = [1,2,3]
+for index in range(len(obj)):
+    value = obj[index]
+    print(value)
+
+#conside enumeration
+obj = [1,2,3]
+for index, value in enumerate(obj):
+    print(index, value) 
+
 # exception
-
 try:
     1 / 0
 except ZeroDivisionError:
-    print("don't divide numbers by 0!")  
+    print("don't divide numbers by 0!")    
 except Exception:
-    print("exception occurred")# exception
-
-try:
-    1 / 0
-except ZeroDivisionError:
-    print("don't divide numbers by 0!")  
-except Exception:
-    print("exception occurred")# exception
-
-try:
-    1 / 0
-except ZeroDivisionError:
-    print("don't divide numbers by 0!")  
-except Exception:
-    print("exception occurred")  
+    print("ex")
