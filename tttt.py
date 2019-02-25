2e177256abbe903eeb47a207d47b4ea81ce56511
Louis Carnec <louis@codacy.com>
2019-02-25 11:29:00 +0000

    more issues

--- tttt.py
+++ tttt.py
@@ -22,6 +22,63 @@ def something():
     print("%(PARG_1)d %(PARG_2)d" % {'PARG_1': 1, 'PARG_2':2, 'PARG_3':3})
 
 
+#adding extra keys
+print("%(PARG_1)d %(PARG_2)d" % {'PARG_1': 1, 'PARG_2':2, 'PARG_3':3})
+def something():
+    #adding extra keys
+    print("%(PARG_1)d %(PARG_2)d" % {'PARG_1': 1, 'PARG_2':2, 'PARG_3':3})
+    #adding extra keys
+    print("%(PARG_1)d %(PARG_2)d" % {'PARG_1': 1, 'PARG_2':2, 'PARG_3':3})
+    #adding extra keys
+    print("%(PARG_1)d %(PARG_2)d" % {'PARG_1': 1, 'PARG_2':2, 'PARG_3':3})
+    #adding extra keys
+    print("%(PARG_1)d %(PARG_2)d" % {'PARG_1': 1, 'PARG_2':2, 'PARG_3':3})
+    #adding extra keys
+    print("%(PARG_1)d %(PARG_2)d" % {'PARG_1': 1, 'PARG_2':2, 'PARG_3':3})
+    #adding extra keys
+    print("%(PARG_1)d %(PARG_2)d" % {'PARG_1': 1, 'PARG_2':2, 'PARG_3':3})
+    #adding extra keys
+    print("%(PARG_1)d %(PARG_2)d" % {'PARG_1': 1, 'PARG_2':2, 'PARG_3':3})
+
+
+#adding extra keys
+print("%(PARG_1)d %(PARG_2)d" % {'PARG_1': 1, 'PARG_2':2, 'PARG_3':3})
+def something():
+    #adding extra keys
+    print("%(PARG_1)d %(PARG_2)d" % {'PARG_1': 1, 'PARG_2':2, 'PARG_3':3})
+    #adding extra keys
+    print("%(PARG_1)d %(PARG_2)d" % {'PARG_1': 1, 'PARG_2':2, 'PARG_3':3})
+    #adding extra keys
+    print("%(PARG_1)d %(PARG_2)d" % {'PARG_1': 1, 'PARG_2':2, 'PARG_3':3})
+    #adding extra keys
+    print("%(PARG_1)d %(PARG_2)d" % {'PARG_1': 1, 'PARG_2':2, 'PARG_3':3})
+    #adding extra keys
+    print("%(PARG_1)d %(PARG_2)d" % {'PARG_1': 1, 'PARG_2':2, 'PARG_3':3})
+    #adding extra keys
+    print("%(PARG_1)d %(PARG_2)d" % {'PARG_1': 1, 'PARG_2':2, 'PARG_3':3})
+    #adding extra keys
+    print("%(PARG_1)d %(PARG_2)d" % {'PARG_1': 1, 'PARG_2':2, 'PARG_3':3})
+
+
+#adding extra keys
+print("%(PARG_1)d %(PARG_2)d" % {'PARG_1': 1, 'PARG_2':2, 'PARG_3':3})
+def something():
+    #adding extra keys
+    print("%(PARG_1)d %(PARG_2)d" % {'PARG_1': 1, 'PARG_2':2, 'PARG_3':3})
+    #adding extra keys
+    print("%(PARG_1)d %(PARG_2)d" % {'PARG_1': 1, 'PARG_2':2, 'PARG_3':3})
+    #adding extra keys
+    print("%(PARG_1)d %(PARG_2)d" % {'PARG_1': 1, 'PARG_2':2, 'PARG_3':3})
+    #adding extra keys
+    print("%(PARG_1)d %(PARG_2)d" % {'PARG_1': 1, 'PARG_2':2, 'PARG_3':3})
+    #adding extra keys
+    print("%(PARG_1)d %(PARG_2)d" % {'PARG_1': 1, 'PARG_2':2, 'PARG_3':3})
+    #adding extra keys
+    print("%(PARG_1)d %(PARG_2)d" % {'PARG_1': 1, 'PARG_2':2, 'PARG_3':3})
+    #adding extra keys
+    print("%(PARG_1)d %(PARG_2)d" % {'PARG_1': 1, 'PARG_2':2, 'PARG_3':3})
+
+
 #conside enumeration
 obj = [1,2,3]
 for index, value in enumerate(obj):
@@ -52,6 +109,20 @@ except ZeroDivisionError:
     print("don't divide numbers by 0!")    
 except Exception:
     print("ex")
+
+try:
+    1 / 0
+except ZeroDivisionError:
+    print("don't divide numbers by 0!")    
+except Exception:
+    print("ex")
+
+try:
+    1 / 0
+except ZeroDivisionError:
+    print("don't divide numbers by 0!")    
+except Exception:
+    print("ex")        
 
 # exception
 try:
