8e18159e1cdb6997711669206c32f1618e09c037
Louis Carnec <louis@codacy.com>
2019-02-28 15:10:25 +0000

    add duplicatin

--- tttt.py
+++ tttt.py
@@ -9,6 +9,7 @@ print("%(PARG_1)d %(PARG_2)d" % {'PARG_1': 1, 'PARG_2':2, 'PARG_3':3})
 def something():
     #adding extra keys
     print("%(PARG_1)d %(PARG_2)d" % {'PARG_1': 1, 'PARG_2':2, 'PARG_3':3})
+
 #conside enumeration
 obj = [1,2,3]
 for index in range(len(obj)):
@@ -20,11 +21,27 @@ obj = [1,2,3]
 for index in range(len(obj)):
     value = obj[index]
     print(value)
+#conside enumeration
+obj = [1,2,3]
+for index in range(len(obj)):
+    value = obj[index]
+    print(value)
+#conside enumeration
+obj = [1,2,3]
+for index in range(len(obj)):
+    value = obj[index]
+    print(value)        
 
 #conside enumeration
 obj = [1,2,3]
 for index, value in enumerate(obj):
     print(index, value) 
+for index, value in enumerate(obj):
+    print(index, value) 
+
+
+for index, value in enumerate(obj):
+    print(index, value)         
 
 # exception
 try:
