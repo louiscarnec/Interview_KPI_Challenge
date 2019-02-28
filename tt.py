40000630f7a8ebe68492e7c0401956805472f182
Louis Carnec <louis@codacy.com>
2019-02-28 15:14:45 +0000

    more dupl

--- tt.py
+++ tt.py
@@ -2,24 +2,21 @@
 
 
 
-imprt chicken
-imprt chicken
-imprt chicken
-imprt chicken
-imprt chicken
-imprt chicken
-imprt chicken
-imprt chicken
-imprt chicken
-imprt chicken
-imprt chicken
-imprt chicken
+import pandas
 
 import pandas
 
 if (var ==== 2):
     print var
 
+
+
 // this is not a comment
 #this is a comment    
 pandas.read_csv()
+
+if (var ==== 2):
+    print var
+
+if (var ==== 2):
+    print var    
