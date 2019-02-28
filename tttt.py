9d61f8beddc4c6022aca91d0ec7dfab4307b0927
Louis Carnec <louis@codacy.com>
2019-02-28 10:11:27 +0000

    new and fixed

--- tttt.py
+++ tttt.py
@@ -16,48 +16,6 @@ def something():
     print("%(PARG_1)d %(PARG_2)d" % {'PARG_1': 1, 'PARG_2':2, 'PARG_3':3})
     #adding extra keys
 
-
-
-def something():
-    #adding extra keys
-    print("%(PARG_1)d %(PARG_2)d" % {'PARG_1': 1, 'PARG_2':2, 'PARG_3':3})
-    #adding extra keys
-    print("%(PARG_1)d %(PARG_2)d" % {'PARG_1': 1, 'PARG_2':2, 'PARG_3':3})
-    #adding extra keys
-    print("%(PARG_1)d %(PARG_2)d" % {'PARG_1': 1, 'PARG_2':2, 'PARG_3':3})
-    #adding extra keys
-    print("%(PARG_1)d %(PARG_2)d" % {'PARG_1': 1, 'PARG_2':2, 'PARG_3':3})
-    #adding extra keys
-    # 
-    # 
-   
-   
-   def something():
-    #adding extra keys
-    print("%(PARG_1)d %(PARG_2)d" % {'PARG_1': 1, 'PARG_2':2, 'PARG_3':3})
-    #adding extra keys
-    print("%(PARG_1)d %(PARG_2)d" % {'PARG_1': 1, 'PARG_2':2, 'PARG_3':3})
-    #adding extra keys
-    print("%(PARG_1)d %(PARG_2)d" % {'PARG_1': 1, 'PARG_2':2, 'PARG_3':3})
-    #adding extra keys
-    print("%(PARG_1)d %(PARG_2)d" % {'PARG_1': 1, 'PARG_2':2, 'PARG_3':3})
-    #adding extra keys #     
-#conside enumeration
-obj = [1,2,3]
-for index, value in enumerate(obj):
-    print(index, value) 
-
-
-def something():
-    #adding extra keys
-    print("%(PARG_1)d %(PARG_2)d" % {'PARG_1': 1, 'PARG_2':2, 'PARG_3':3})
-    #adding extra keys
-    print("%(PARG_1)d %(PARG_2)d" % {'PARG_1': 1, 'PARG_2':2, 'PARG_3':3})
-    #adding extra keys
-    print("%(PARG_1)d %(PARG_2)d" % {'PARG_1': 1, 'PARG_2':2, 'PARG_3':3})
-    #adding extra keys
-    print("%(PARG_1)d %(PAR
-
 #conside enumeration
 obj = [1,2,3]
 for index in range(len(obj)):
@@ -78,7 +36,15 @@ for index, value in enumerate(obj):
 # exception
 try:
     1 / 0
+except Exception:
+print("ex")
 except ZeroDivisionError:
     print("don't divide numbers by 0!")    
+
+
+try:
+    1 / 0
 except Exception:
     print("ex")
+except ZeroDivisionError:
+    print("don't divide numbers by 0!")  
