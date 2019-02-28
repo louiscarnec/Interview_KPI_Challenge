cd43eba96563039d3b3569a228f065cc380fa3f0
Louis Carnec <louis@codacy.com>
2019-02-28 09:34:13 +0000

    more issues

--- tttt.py
+++ tttt.py
@@ -48,6 +48,16 @@ for index, value in enumerate(obj):
     print(index, value) 
 
 
+def something():
+    #adding extra keys
+    print("%(PARG_1)d %(PARG_2)d" % {'PARG_1': 1, 'PARG_2':2, 'PARG_3':3})
+    #adding extra keys
+    print("%(PARG_1)d %(PARG_2)d" % {'PARG_1': 1, 'PARG_2':2, 'PARG_3':3})
+    #adding extra keys
+    print("%(PARG_1)d %(PARG_2)d" % {'PARG_1': 1, 'PARG_2':2, 'PARG_3':3})
+    #adding extra keys
+    print("%(PARG_1)d %(PAR
+
 #conside enumeration
 obj = [1,2,3]
 for index in range(len(obj)):
